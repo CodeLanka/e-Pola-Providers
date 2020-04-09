c8f440c0ab430c27a0b7083c311da1eb9e25e428
Suvink <hello@suvink.me>
2020-04-09 21:29:54 +0530

    Added base styling and pages

--- src/routes/Account/components/AccountForm/AccountForm.js
+++ src/routes/Account/components/AccountForm/AccountForm.js
@@ -6,8 +6,13 @@ import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import { validateEmail } from 'utils/form'
-import ProviderDataForm from '../ProviderDataForm'
 import styles from './AccountForm.styles'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import PermIdentityIcon from '@material-ui/icons/PermIdentity'
+import MailOutlineIcon from '@material-ui/icons/MailOutline'
+import PhoneIcon from '@material-ui/icons/Phone'
+import FingerprintIcon from '@material-ui/icons/Fingerprint'
+import RoomIcon from '@material-ui/icons/Room'
 
 const useStyles = makeStyles(styles)
 
@@ -27,46 +32,103 @@ function AccountForm({ account, onSubmit }) {
   return (
     <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.fields}>
+        <Typography variant="h4" className={classes.settingsTitle}>
+          Account Settings
+        </Typography>
         <TextField
           name="displayName"
           label="Display Name"
           margin="normal"
           fullWidth
+          variant="outlined"
+          inputRef={register}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <PermIdentityIcon />
+              </InputAdornment>
+            )
+          }}
         />
         <TextField
+          name="contactNumber"
+          label="Contact Number"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          inputRef={register}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <PhoneIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          name="identityNumber"
+          label="NIC Number"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          inputRef={register}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <FingerprintIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          name="address"
+          label="Delivery Address"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          inputRef={register}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <RoomIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          label="Your Email"
           type="email"
           name="email"
           placeholder="email"
           margin="normal"
           fullWidth
+          variant="outlined"
           inputRef={register({
             required: true,
             validate: validateEmail
           })}
           error={!!errors.email}
+          errorStyle={{ color: 'red' }}
           helperText={errors.email && 'Email must be valid'}
-        />
-        <TextField
-          name="avatarUrl"
-          label="Avatar Url"
-          inputRef={register}
-          margin="normal"
-          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <MailOutlineIcon />
+              </InputAdornment>
+            )
+          }}
         />
       </div>
-      {!!account && !!account.providerData && (
-        <div>
-          <Typography variant="h6">Linked Accounts</Typography>
-          <ProviderDataForm providerData={account.providerData} />
-        </div>
-      )}
-      <Button
-        color="primary"
-        type="submit"
-        variant="contained"
-        disabled={isSubmitting || !isValid}>
-        {isSubmitting ? 'Saving' : 'Save'}
-      </Button>
+      <div className={classes.saveDiv}>
+        <Button
+          className={classes.btnSave}
+          color="primary"
+          type="submit"
+          variant="outlined"
+          disabled={isSubmitting || !isValid}>
+          {isSubmitting ? 'Saving' : 'Save'}
+        </Button>
+      </div>
     </form>
   )
 }
