373710c5ca1e600c710d972bebc2608fc828606e
agentmilindu <agentmilindu@gmail.com>
2020-04-11 22:20:37 +0530

    Backport from e-Pola

--- src/routes/Needs/components/NeedsHero/NeedsHero.js
+++ src/routes/Needs/components/NeedsHero/NeedsHero.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ContentAddCircle from '@material-ui/icons/AddCircle'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
-import styles from './NewNeedTile.styles'
+import styles from './NeedsHero.styles'
 import { Typography } from '@material-ui/core'
 import { Trans } from 'react-i18next'
 
 const useStyles = makeStyles(styles)
 
-function NewNeedTile({ onClick }) {
+function NeedsHero({ onClick }) {
   const classes = useStyles()
 
   return (
     <Paper className={classes.root} onClick={onClick}>
-      <ContentAddCircle className={classes.newIcon} />
-      <Typography variant="h6" color="secondary">
-        <Trans>Request new item</Trans>
+      <Typography variant="h4">Hello User!</Typography>
+      <Typography variant="h6">
+        <Trans>Your Needs List</Trans>
       </Typography>
     </Paper>
   )
 }
 
-NewNeedTile.propTypes = {
+NeedsHero.propTypes = {
   onClick: PropTypes.func
 }
 
-export default NewNeedTile
+export default NeedsHero
